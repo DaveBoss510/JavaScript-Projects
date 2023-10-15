a66ea8c38e52dbb1d76e2be4e0c55ed03c45f224
DaveBoss510 <145558801+DaveBoss510@users.noreply.github.com>
2023-10-15 09:46:38 +0100

    Project2_functions assignment
    making functions for onclick with html and JS files.

---
+++ Basic_JavaScript_Projects/Projects2_functions/JS/Main.js
@@ -0,0 +1,13 @@
+//myFunction concatenates two strings with ouput name "sentence" and sends to id "concatenate" in html page to print as clickable link and text after event handler click.
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
+//secondFunction is a second function with id="link"
+function secondFunction() {
+    var sentence = "ok, this is number two";
+    sentence += " lets see how it goes";
+    document.getElementById("second").innerHTML = sentence;
+}
