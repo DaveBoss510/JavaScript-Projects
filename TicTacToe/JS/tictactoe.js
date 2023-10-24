93c9b5565315a18b23caa8f4face4f8aec15fa52
DaveBoss510 <145558801+DaveBoss510@users.noreply.github.com>
2023-10-24 21:28:07 +0100

    update

--- TicTacToe/JS/tictactoe.js
+++ TicTacToe/JS/tictactoe.js
@@ -19,7 +19,7 @@ function placeXOrO(squareNumber) {
             //Active player may only be 'X' or 'O' so, if not 'X' it must be 'O'
         }    else {
             //If activePlayer is equal to 'O', the o.png is placed in HTML
-            select.style.backgroundImage = 'Url("images/o.png")';
+            select.style.backgroundImage = 'url("images/o.png")';
         }
         //squareNumber and activePlayer are concatenated together and added to array.
         selectedSquares.push(squareNumber + activePlayer);
@@ -67,7 +67,6 @@ function placeXOrO(squareNumber) {
             };
         }
     }
-
 }
 
 //This function parses the selectedSquares array to search for win conditions.
@@ -76,7 +75,7 @@ function checkWinConditions() {
     //X 0, 1, 2 condition.
     if (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100) }
     // X 3, 4, 5.
-    else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 508) }
+    else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304) }
     // X 6, 7, 8 condition.
     else if (arrayIncludes('6X', '7X', '8X')) {drawWinLine(50, 508, 558, 508) }
     // X 0, 3, 6 condition.
